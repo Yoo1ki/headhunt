3fd2ff6a6d96a2580158127cc5e4652c61ae206c
Bagus Pangestu <baguspangestu44@gmail.com>
2026-04-24 17:38:59 +0700

    Fix change locale session

--- src/components/common/SwitchLocale.tsx
+++ src/components/common/SwitchLocale.tsx
@@ -1,23 +1,32 @@
 "use client";
 
 import { CONFIG } from "@/config";
-import { useRouter, usePathname } from "@/i18n/navigation";
+import { usePathname } from "@/i18n/navigation";
 import { useLocale } from "next-intl";
 import { useState, useRef, useEffect } from "react";
 import { FaGlobeAsia } from "react-icons/fa";
 import { TiArrowSortedDown } from "react-icons/ti";
+import { useImportStore } from "@/store/useImportStore";
+import clsx from "clsx";
 
 export const SwitchLocale = () => {
-  const router = useRouter();
   const pathname = usePathname();
   const locale = useLocale();
+  const isImporting = useImportStore((s) => s.isImporting);
   const [open, setOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
 
   const handleChange = (newLocale: string) => {
     const hash = window.location.hash;
-    router.replace(pathname + hash, { locale: newLocale });
-    setOpen(false);
+
+    const segments = pathname.split("/");
+    if (CONFIG.locales.some((l) => l.id === segments[1])) {
+      segments[1] = newLocale;
+    } else {
+      segments.splice(1, 0, newLocale);
+    }
+
+    window.location.href = segments.join("/") + hash;
   };
 
   useEffect(() => {
@@ -36,11 +45,19 @@ export const SwitchLocale = () => {
     };
   }, []);
 
+  const stateClass = isImporting
+    ? "bg-neutral-600 text-white/60"
+    : "cursor-pointer";
+
   return (
     <div className="relative" ref={dropdownRef}>
       <button
+        disabled={isImporting}
         onClick={() => setOpen(!open)}
-        className="w-36 px-3 py-1.5 rounded-xl bg-white/5 hover:bg-white/10 flex justify-between items-center gap-2 cursor-pointer duration-300"
+        className={clsx(
+          "w-36 px-3 py-1.5 rounded-xl bg-white/5 hover:bg-white/10 flex justify-between items-center gap-2 duration-300",
+          stateClass,
+        )}
       >
         <div className="flex items-center gap-2">
           <FaGlobeAsia />
