64ea76e03d2b344d211876fd3285c1382b738ab6
Bagus Pangestu <baguspangestu44@gmail.com>
2026-04-29 01:12:54 +0700

    Redirect Not Found Page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+import { getLocale } from "next-intl/server";
+import { redirect } from "next/navigation";
+
+export default async function NotFound() {
+  const locale = await getLocale();
+
+  redirect(`/${locale}`);
+}
