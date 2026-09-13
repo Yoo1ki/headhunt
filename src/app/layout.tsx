7fdd968cfae0e700d63c269310d007ce443905f4
Bagus Pangestu <baguspangestu44@gmail.com>
2026-09-14 02:08:40 +0700

    feat: add Google Analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { getLocale } from 'next-intl/server';
 import { Roboto } from 'next/font/google';
 import { CONFIG } from '@/config';
 import type { Metadata } from 'next';
+import { GoogleAnalytics } from '@next/third-parties/google';
 import '@/app/globals.css';
 
 const roboto = Roboto({
@@ -21,12 +22,14 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   const locale = await getLocale();
+  const googleAnalyticsId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
 
   return (
     <html lang={locale}>
       <body className={`${roboto.variable} lg:flex lg:h-screen lg:flex-col`}>
         {children}
       </body>
+      {googleAnalyticsId && <GoogleAnalytics gaId={googleAnalyticsId} />}
     </html>
   );
 }
